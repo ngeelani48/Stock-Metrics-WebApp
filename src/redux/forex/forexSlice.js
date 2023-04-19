44b43fa65620803cb9d4a44d3c0c48b492132547
Muhammed Nafees Geelani <engnrnafees@gmail.com>
2023-04-19 19:38:47 +0300

    Style the components

--- src/redux/forex/forexSlice.js
+++ src/redux/forex/forexSlice.js
@@ -1,7 +1,8 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
-const fetchUrl = 'https://financialmodelingprep.com/api/v3/fx?apikey=6739a7534e62f4b6274554e7e1dfb880';
+// const fetchUrl = 'https://financialmodelingprep.com/api/v3/fx?apikey=6739a7534e62f4b6274554e7e1dfb880';
 
+const fetchUrl = 'https://economia.awesomeapi.com.br/json/last/USD-BRL,EUR-BRL,BTC-BRL,AUD-BRL,GBP-USD,ARS-USD,JPY-USD,CHF-USD,CNY-USD,ILS-USD,BTC-USD,LTC-USD,ETH-USD,XRP-USD,BRL-EUR,USD-EUR,CAD-EUR,GBP-EUR';
 const initialState = {
   forexs: [],
   isLoading: false,
@@ -11,7 +12,8 @@ export const getForex = createAsyncThunk('forex/getForex', async () => {
   try {
     const dataStream = await fetch(fetchUrl);
     const data = await dataStream.json();
-    return data;
+    const newdata = Object.entries(data);
+    return newdata;
   } catch (error) {
     return error;
   }
